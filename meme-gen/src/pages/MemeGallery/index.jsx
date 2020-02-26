cfaa25cd4f451f0aae0a44d7c2662a6a9a10ae22
Daniel Truong <ddtruong06@gmail.com>
2020-02-26 15:37:14 -0800

    Updated React to avoid having to go into the front-end code

--- meme-gen/src/pages/MemeGallery/index.jsx
+++ meme-gen/src/pages/MemeGallery/index.jsx
@@ -5,16 +5,49 @@ import aight from '../../images/aight-imma-out.png';
 import badMeme from '../../images/badmeme.jpeg';
 import MemeCard from '../../components/MemeCard';
 
-const MemeGallery = (props) => {
-    return (
-        <div id="content">
-            <h2>Meme Gallery</h2>
-            <div className="memes">
-                <MemeCard image={aight} creator="Daniel Truong" likes={1} likedStatus={false}/>
-                <MemeCard image={badMeme} creator="ur mom" likes={0} likedStatus={false} />
+class MemeGallery extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            memeArray: null
+        };
+        fetch("/getmeme")
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    memeArray: response
+                });
+            });
+    }
+    
+    render() {
+        if (!this.state.memeArray) {
+            return <div/>
+        }
+        const memes = this.state.memeArray.map((meme) => 
+            <MemeCard key={meme._id} id={meme._id} image={meme.image} creator={meme.creator} likes={meme.likes} likedStatus={meme.isLiked} />
+        )
+        return (
+            <div id="content">
+                <h2>Meme Gallery</h2>
+                <div className="memes">
+                    {memes}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+// const MemeGallery = (props) => {
+//     return (
+//         <div id="content">
+//             <h2>Meme Gallery</h2>
+//             <div className="memes">
+//                 <MemeCard image={aight} creator="Daniel Truong" likes={1} likedStatus={false}/>
+//                 <MemeCard image={badMeme} creator="ur mom" likes={0} likedStatus={false} />
+//             </div>
+//         </div>
+//     );
+// }
 
 export default MemeGallery;
